33ff6aaf882ba0d764773ee89fae5245a0b56c1e
Mr.T <72496013+Mr-T-13@users.noreply.github.com>
2020-12-04 17:31:17 +0100

    PopUp Generator
    1.Haces un panel, le metes el fondo los botones etc
    2.Lo guardas como prefab
    3.Asocias este script al boton y en el componente script asocias el prefab

---
+++ src/ConnectMind/Assets/Scripts/PopUpSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpSystem : MonoBehaviour
+{
+    
+    public GameObject prefab;
+    bool active = false;
+    private Transform canvas;
+    private GameObject panel;
+
+    private void Awake()
+    {
+        canvas = transform.parent;
+        panel = Instantiate(prefab,canvas);
+    }
+
+    public void popUpWork()
+    {
+        if (active == false)
+        {
+            panel.SetActive(true);
+            active = true;
+        }else if (active == true)
+        {
+            panel.SetActive(false);
+            active = false;
+        }
+    }
+
+}
