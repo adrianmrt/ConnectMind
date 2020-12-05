09be0bfc77ef4466e41ad9946e2aa8f73e50f62d
Mr.T <72496013+Mr-T-13@users.noreply.github.com>
2020-12-05 11:55:03 +0100

    Pantallas Progreso y Entrenamiento añadidas

---
+++ src/ConnectMind/Assets/Scripts/CambioScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioScene : MonoBehaviour
+{
+    public string escena;
+
+    public void cambioEscena()
+    {
+        SceneManager.LoadScene(escena);
+    }
+}
